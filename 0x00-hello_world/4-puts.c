a9cf810dbc1051ef9678211f42c74d132df68c13
oumar17 <oumar7sacko@gmail.com>
2022-09-15 09:51:10 -0700

    print programmin is fun

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * print some char to the screen 
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	char  words [] = "\"Programming is like building a multilingual puzzle";
+	puts(words);
+	return (0);
+
+}
