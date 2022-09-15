a5a492685ddc2ddefa30d2a60acde8dca6e456a6
oumar17 <oumar7sacko@gmail.com>
2022-09-15 11:31:35 -0700

    hot fix of sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
 */
 int main(void)
 {
-	puts("Size of a char: ", sizeof(char));
-	puts("Size of an int: ", sizeof(int));
-	puts("Size of a long int: ", sizeof(long int));
-	puts("Size of a long loint: ", sizeof(long));
-	puts("Size of a float: ", sizeof(float));
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 	return (0);
 }
