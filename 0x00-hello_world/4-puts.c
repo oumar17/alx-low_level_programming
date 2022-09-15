e370e2e1798f08eb29eda96ba668479f26d99345
oumar17 <oumar7sacko@gmail.com>
2022-09-15 10:25:23 -0700

    hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- * print some char to the screen 
- * Return: Always 0 (Success)
- **/
+* main - Entry point
+* print some char to the screen
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	char  words [] = "\"Programming is like building a multilingual puzzle";
+	char words[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(words);
 	return (0);
-
 }
