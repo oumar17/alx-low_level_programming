7ff2062d1987ea70770573eff4872f626cfce8cc
oumar17 <oumar7sacko@gmail.com>
2022-09-16 04:52:06 -0700

    multiply two numinterger

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+/**
+* mul - Mutiply 2 input integer
+* @a: first input
+* @b: second input
+* Return: the product
+*/
+int mul(int a, int b)
+{
+	int ret = 0;
+
+	ret = a * b;
+
+	return (ret);
+}
