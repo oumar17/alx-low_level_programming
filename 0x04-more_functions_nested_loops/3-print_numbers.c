a1e1848040d01cc9f129ee298b8d0288cd830710
oumar17 <oumar7sacko@gmail.com>
2022-09-16 05:47:13 -0700

    print numbr

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,21 @@
 /**
 * print_numbers - print number  0 to 9
-*
+* @c: internal variable tohold number
 * Return: void
 */
+
+int _putchar(char c);
+
 void  print_numbers(void)
 {
-	int i = 0;
-	char c = "\n";
-	while (i++ <= 10)
+	int i = 48;
+
+	char c = '\n';
+
+	while (i++ <= 56)
 	{
 		c = (char) i;
-		_putchar( c );
+		_putchar(c);
 	}
 	_putchar('\n');
 }
