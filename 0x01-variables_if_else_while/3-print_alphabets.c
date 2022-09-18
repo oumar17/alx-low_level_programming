5223cc79e75dd5b89d4615908cc6a2830011cc93
oumar17 <oumar7sacko@gmail.com>
2022-09-18 09:00:06 -0700

    alphabet with cases

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * print print letter of alphabetin lower case
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char chr;
+
+	for (chr = 'a'; chr <= 'z'; chr++)
+		putchar(chr);
+	for (chr = 'A'; chr <= 'Z'; chr++)
+		putchar(chr);
+	putchar('\n');
+	return (0);
+}
