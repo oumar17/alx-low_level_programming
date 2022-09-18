a750dfeb560dac521671c79d81550250c918e5fa
oumar17 <oumar7sacko@gmail.com>
2022-09-18 09:07:29 -0700

    print digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * print digit
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char chr;
+
+	for (chr = '0'; chr <= '9'; chr++)
+		putchar(chr);
+	putchar('\n');
+	return (0);
+}
