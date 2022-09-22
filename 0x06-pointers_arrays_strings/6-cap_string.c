5cbb62a868c4539ee9f25dce6f2c6344a13a09a6
oumar17 <oumar7sacko@gmail.com>
2022-09-22 05:34:58 -0700

    capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+* *cap_string - capitalize wordtwo string
+*@*str: input char (*) dest
+*@c: input to check
+*Return:char * value
+*/
+int check(char c);
+char *cap_string(char *str)
+{
+	int j;
+
+	for (j = 0; str[j] != '\0'; j++)
+	{
+		if (check(str[j]) && str[j + 1] >= 'a' && str[j + 1] <= 'z')
+		{
+			str[j + 1] = str[j + 1] - 32;
+			j++;
+		}
+	}
+	return (str);
+}
+
+
+/**
+ *check - compare  char
+ * @c: input char
+ *Return: ret 1 if match
+ */
+
+int  check(char c)
+{
+	int i, ret;
+	char set[] = " \t\n ,, ;, ., !, ?, \"(){}";
+
+	ret = 0;
+	for (i = 0; i <= 28; i++)
+		if (set[i] == c)
+			ret = 1;
+	return (ret);
+
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H_
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
+char *cap_string(char *);
 char *string_toupper(char *);
 char *_strncat(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
