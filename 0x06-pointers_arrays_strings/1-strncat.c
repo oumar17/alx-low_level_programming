0c2c7698216475207f3ad6cc693e32002340a9be
oumar17 <oumar7sacko@gmail.com>
2022-09-22 01:04:21 -0700

    not fxed

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,7 +18,7 @@ char *_strncat(char *dest, char *src, int n)
 		dest[j + i] = src[j];
 		j++;
 	} while (j < n && j < t);
-/*	dest[j + j + 1] = '\0'; */
+	dest[j + j + 1] = '\0';
 	return (dest);
 }
 
