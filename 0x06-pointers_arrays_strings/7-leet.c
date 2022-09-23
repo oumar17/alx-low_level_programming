35e00cfe836e44bda3891675210475824bb11037
oumar17 <oumar7sacko@gmail.com>
2022-09-23 00:57:53 -0700

    coding made fun

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,38 +1,21 @@
 #include <stdio.h>
 /**
 * *leet - encode text wordtwo string
-*@*str: input char (*) dest
-*@c: input to check
-*Return:char * value
+*@str: input char (*) to leet
+*Return:char (*) str
 */
-int check(char c);
 char *leet(char *str)
 {
-	int j;
+	int j, i;
+	char set[] = "aAeEoOtTlL4433007711";
 
 	for (j = 0; str[j] != '\0'; j++)
 	{
-		if (check(str[j]) >= 0)
-			str[j] = (char) check(str[j]);
+		for (i = 0; i < 10; i++)
+		{
+			if (str[j] == set[i])
+			str[j] = (char)  set[i + 10];
+		}
 	}
 	return (str);
-}
-
-
-/**
- *check - compare  char
- * @c: input char
- *Return: ret 1 if match
- */
-int  check(char c)
-{
-	int i, ret;
-	char set[] = "aAeEoOtTlL4433007711";
-
-	ret = -1;
-	for (i = 0; i < 10; i++)
-		if (set[i] == c)
-			ret = set[i + 10];
-	return (ret);
-
 }
