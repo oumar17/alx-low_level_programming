a6eabbe88f4897ef71a933bf1d68073f273564b3
oumar17 <oumar7sacko@gmail.com>
2022-09-23 09:58:59 -0700

    capital is the thing to solve

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
-* *cap_string - capitalize wordtwo string
+* *cap_string - capitalize
 *@str: input char (*) dest
 *Return:char * value
 */
 char *cap_string(char *str)
 {
 	int j, i;
-	char set[] = "\n,;\t.!?\"(){}";
+	char set[] = "\n,;\t. !?\"(){}";
 
 	for (j = 0; str[j] != '\0'; j++)
 	{
