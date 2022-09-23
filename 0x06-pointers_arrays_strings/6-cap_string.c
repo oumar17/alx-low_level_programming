40a1dacbf10b35b813eeb71b69498652859a9449
oumar17 <oumar7sacko@gmail.com>
2022-09-23 01:14:20 -0700

    capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,33 +1,28 @@
 #include <stdio.h>
 /**
 * *cap_string - capitalize wordtwo string
-*@*str: input char (*) dest
-*@c: input to check
+*@str: input char (*) dest
 *Return:char * value
 */
-int check(char c);
 char *cap_string(char *str)
 {
-	int j;
+	int j, i;
 	char set[] = "\n ,, ;, ., !, ?, \"(){}";
-	int i, ret;
 
 	for (j = 0; str[j] != '\0'; j++)
-	
 	{
-		
-			if (str[j] == '\t')
-			{	str[j] = ' ';
-				continue
-			}
-			else if  (str[j] >= 'a' && str[j] <= 'z')
+
+		if (str[j] == '\t')
+		{	str[j] = ' ';
+			continue;
+		}
+		for (i = 0; i <= 22; i++)
+		{
+			if  (set[i] == str[j] &&  str[j + 1] >= 'a' && str[j + 1] <= 'z')
 			{
-				for (i = 0; i <= 22; i++)
-					if (set[i] == str[j])
-			if (str[j] == '\t')
-				str[j] = ' ';
-			str[j + 1] = str[j + 1] - 32;
-			j++;
+				str[j + 1] = str[j + 1] - 32;
+				j++;
+			}
 		}
 	}
 	return (str);
