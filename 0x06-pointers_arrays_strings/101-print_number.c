c2ec14015fe16b5c6d2be10418549736f2edb9a2
oumar17 <oumar7sacko@gmail.com>
2022-09-24 05:39:50 -0700

    print number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,17 +1,16 @@
 /**
 *print_number - print input number
-*@c: input to _putchar
+*@c: int input
+*
 *Return: returns 0
 */
 
 int _putchar(char c);
-
 void print_number(int n)
 {
-	int i, j;
-	char temp[11] = "";
+	int i, temp;
 
-	i = 0;
+	temp = 0;
 	if (n == 0)
 		_putchar('0');
 	else if (n < 0)
@@ -21,11 +20,12 @@ void print_number(int n)
 	}
 	for (i = 0; n % 10 || n / 10 ; i++)
 	{
-		temp[i] = n % 10;
+		temp = (temp * 10) + (n % 10);
 		n = n / 10;
 	}
-	for (j = i - 1; j >= 0; j--)
+	for (; temp / 10 || temp % 10;)
 	{
-		_putchar(temp[j] + '0');
+		_putchar((temp % 10) + '0');
+		temp = temp / 10;
 	}
 }
